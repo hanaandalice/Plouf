af9822c840f315474f4603201371a824b16dfa5e
PororiLi <xiozioll@gmail.com>
2021-05-06 17:15:40 +0900

    HomeFragment UI , onClick event

--- app/src/main/java/com/example/plouf/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/plouf/ui/home/HomeFragment.java
@@ -18,6 +18,8 @@ import androidx.lifecycle.ViewModelProvider;
 
 import com.example.plouf.R;
 
+import org.w3c.dom.Text;
+
 import static android.content.ContentValues.TAG;
 
 public class HomeFragment extends Fragment implements View.OnClickListener{
@@ -25,22 +27,40 @@ public class HomeFragment extends Fragment implements View.OnClickListener{
     private HomeViewModel homeViewModel;
     public String msgWater;
     public ImageView img_water;
+    public ImageView img_pee;
+    public ImageView img_feces;
+    public ImageView img_drink;
+    public TextView tv_progress;
+
+
+
     private Context context;
+    public TextView tv_waterAmount;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         homeViewModel =
                 new ViewModelProvider(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
-        ImageView img_water = root.findViewById(R.id.img_water);
-        Log.d(TAG, "onCreateView: ");
+
+//        initHome();
+
+        //init home
+        tv_progress = root.findViewById(R.id.tv_progress);
+        img_water = root.findViewById(R.id.img_water);
+        tv_waterAmount = root.findViewById(R.id.tv_waterAmount);
+        img_drink = root.findViewById(R.id.img_drink);
+        img_feces = root.findViewById(R.id.img_feces);
+        img_pee = root.findViewById(R.id.img_pee);
+
+
         homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
-                textView.setText(s);
+                tv_progress.setText(s);
             }
         });
+        tv_waterAmount.setText("마신 물 양");
 
         context = container.getContext();
 
@@ -52,19 +72,45 @@ public class HomeFragment extends Fragment implements View.OnClickListener{
             }
         });
 
-        img_water.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(context, msgWater, Toast.LENGTH_SHORT).show();
-                Log.d(TAG, "setOnClick: "+msgWater);
-            }
-        });
+        img_water.setOnClickListener(this);
+        img_pee.setOnClickListener(this);
+        img_feces.setOnClickListener(this);
+        img_drink.setOnClickListener(this);
+
 
         return root;
     }
 
+    //onclick event
     @Override
     public void onClick(View v) {
+       switch (v.getId()){
+            case R.id.img_water :
+                Toast.makeText(context, msgWater, Toast.LENGTH_SHORT).show();
+                Log.d(TAG, "setOnClick: "+msgWater);
+                break;
+            case R.id.img_pee :
+                Toast.makeText(context, "pee", Toast.LENGTH_SHORT).show();
+                Log.d(TAG, "setOnClick: "+img_pee);
+                break;
+            case R.id.img_feces :
+                Toast.makeText(context, "feces", Toast.LENGTH_SHORT).show();
+                Log.d(TAG, "setOnClick: "+img_feces);
+                break;
+            case R.id.img_drink :
+                Toast.makeText(context, "drink", Toast.LENGTH_SHORT).show();
+                Log.d(TAG, "setOnClick: "+img_drink);
+                break;
+
+        }
 
     }
+
+//    public void initHome(){
+//        tv_progress = root.findViewById(R.id.tv_progress);
+//        img_water = root.findViewById(R.id.img_water);
+//        tv_waterAmount = root.findViewById(R.id.tv_waterAmount);
+//        img_drink = root.findViewById(R.id.img_drink);
+//
+//    }
 }
